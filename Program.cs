6fa83e2be834ef272a94ec9b3f072a02d6a1f3e6
MrDavisCompSci <38750430+MrDavisCompSci@users.noreply.github.com>
2025-10-06 10:03:18 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Console template
+using System;
+class Program
+{
+    static void Main()
+    {
+        // Declare an empty list
+
+        // len(clothes)
+        
+        // append(“socks”)
+        
+        // append(“shoes”)
+        
+        // append(“hat”)
+        
+        // append(“socks”)
+        
+        // count(“socks”)
+        
+        // index(“shoes”)
+        
+        // len(clothes)
+        
+        // insert(2, “gloves”)
+        
+        // remove(“socks”)
+        
+        // remove(“shirt”)
+        
+        // append(“socks”)
+        
+        // append(“shorts”)
+        
+        // len(clothes)
+        
+        // index(“gloves”)
+
+    }
+}
+
